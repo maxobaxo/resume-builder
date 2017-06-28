7f15429854c01541140a28b5842aa821f6d7b300
Calla Rudolph and Max Scher <calla@>
2017-06-28 16:24:59 -0700

    experiment with additional class

--- app/app.php
+++ app/app.php
@@ -5,6 +5,10 @@
 
     session_start();
 
+    if(empty($_SESSION['person_info'])) {
+        $_SESSION['person_info'] = array();
+    }
+
     if(empty($_SESSION['list_of_jobs'])) {
         $_SESSION['list_of_jobs'] = array();
     }
@@ -15,7 +19,27 @@
     ));
 
     $app->get('/', function() use ($app) {
-        return $app['twig']->render('jobs.html.twig', array('jobs' => Job::getAll()));
+        return $app['twig']->render('jobs.html.twig', array('person' => Person::getAll()));
+    });
+
+    $app->post('/resume', function() use ($app) {
+        $person = new Person($_POST['name'], $_POST['email']);
+        $person->save();
+        return $app['twig']->render('resume.html.twig', array('new_person' => $person));
+    });
+
+    $app->get('/resume', function() use ($app) {
+        return $app['twig']->render('resume.html.twig', array('jobs' => Job::getAll(), 'new_person' => Person::getAll()));
+    });
+
+    $app->post('/created_job', function() use ($app) {
+        $job = new Job($_POST['company'], $_POST['title'], $_POST['dates'], $_POST['description'], $_POST['image_url']);
+        $job->save();
+        return $app['twig']->render('created_job.html.twig', array('new_job' => $job));
+    });
+
+    $app->post('/deleted_jobs', function() use ($app) {
+        return $app['twig']->render('deleted_jobs.html.twig', array('delete_jobs'=> Job::deleteAll(), 'delete_person' => Person::deleteAll()));
     });
 
     return $app;
--- src/Job.php
+++ src/Job.php
@@ -22,6 +22,38 @@
             return $this->company;
         }
 
+        function setTitle($title) {
+            $this->title = (string) $title;
+        }
+
+        function getTitle() {
+            return $this->title;
+        }
+
+        function setDescription($description) {
+            $this->description = (string) $description;
+        }
+
+        function getDescription() {
+            return $this->description;
+        }
+
+        function setDates($dates) {
+            $this->dates = (string) $dates;
+        }
+
+        function getDates() {
+            return $this->dates;
+        }
+
+        function setImage($image_url) {
+            $this->image_url = (string) $image_url;
+        }
+
+        function getImage() {
+            return $this->image_url;
+        }
+
         function save() {
             array_push($_SESSION['list_of_jobs'], $this);
         }
@@ -32,6 +64,44 @@
 
         static function deleteAll() {
             $_SESSION['list_of_jobs'] = array();
+        }
+    }
+
+    class Person {
+        private $name;
+        private $email;
+
+        function __construct($name, $email) {
+            $this->name = $name;
+            $this->email = $email;
+        }
+
+        function setName($name) {
+            $this->name = (string) $name;
+        }
+
+        function getName() {
+            return $this->name;
+        }
+
+        function setEmail($email) {
+            $this->email = (string) $email;
+        }
+
+        function getEmail() {
+            return $this->email;
+        }
+
+        function save() {
+            array_push($_SESSION['person_info'], $this);
+        }
+
+        static function getAll() {
+            return $_SESSION['person_info'];
+        }
+
+        static function deleteAll() {
+            return $_SESSION['person_info'];
         }
     }
 ?>
